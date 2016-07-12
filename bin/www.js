357bcc08c6605acb59528a36905dd484e34064ff
Akshar Dave <akshar314@gmail.com>
2016-07-12 10:09:42 +0530

    Add Poll & Pollopitons models

--- bin/www.js
+++ bin/www.js
@@ -2,5 +2,8 @@
  * Created by Rampage on 7/11/2016.
  */
 
-require('babel-register');
-var app = require('../server/app.js');
+require('babel-register')
+require('babel-polyfill')
+require('../server/app')
+
+
--- server/app.js
+++ server/app.js
@@ -5,20 +5,47 @@
 
 import express from 'express'
 import db from 'sequelize-connect'
+import path from 'path'
 
 
-async function connect () {
-    await db.connect('andvote_schema','admin','root')
+    async function demo() {
+        db.discover = path.join(__dirname, 'models')
+        db.matcher = function shouldImportModel (modelFileName) {
+            return true
+        }
+        await db.connect('andvote_schema', 'root', 'root')
+    }
 
-}
 
-(async function () {
-    await connect()
+    (async function () {
+    try{
+        await demo()
+    } catch(err){
+        console.log("An error occured connecting to database "+err);
+    }
     const app = express()
     const port =3000
     app.listen(port,()=> console.log('Running on port '+port))
+
 })()
 
+
+   /* var Connection 		= require('sequelize-connect');
+
+    var discover = [__dirname + '/models'];
+    var orm = new Connection(
+        'andvote_schema',
+        'root',
+        '',
+        {
+            dialect: "mysql",
+            port:    3306
+        },
+        discover,
+    )
+        .then(function(instance){
+            console.log("established")
+        });*/
 
 
 
